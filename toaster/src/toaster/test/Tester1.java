166e9a243d29f5c4863183f791ce75a8f353f22e
remote-star <148054685@qq.com>
2014-05-23 13:54:52 +0800

    Change Test
    Change Test

--- toaster/src/toaster/test/Tester1.java
+++ toaster/src/toaster/test/Tester1.java
@@ -4,7 +4,12 @@ public class Tester1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("yeah!");
+		System.out.println("yeah!");
+		System.out.println("yeah!");
+		System.out.println("yeah!");
+		System.out.println("yeah!");
+		System.out.println("yeah!");
 	}
 
 }
